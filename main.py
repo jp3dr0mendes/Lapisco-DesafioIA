d6f238e1cb624fa7d1808b54dd03357fd1a24bfb
joao pedro <joao.albuquerque@irede.org.br>
2023-12-01 14:11:50 -0300

    finishing

--- main.py
+++ main.py
@@ -4,37 +4,66 @@ import cv2 as cv
 
 #load model
 model = YOLO("face-detection-yolov8/best.pt")
+
 # load video
-img_path = 'images.jpg'
+img_path = input('Digite o caminho para o vídeo: ')
+img_path = img_path.split('"')
+img_path = img_path[1]
+video_name = img_path.split('.')
+video_name = video_name[0]+'-edited'+'.'+video_name[1]
+codec = cv.VideoWriter_fourcc(*'mp4v')
 
-results = model(img_path)
-boxes = results[0].boxes
-img = cv.imread(img_path)
+cap = cv.VideoCapture(img_path)
+ret, frame = cap.read()
+cv.resize(frame,(480,640))
 
-position = (20,20)
-font = cv.FONT_HERSHEY_COMPLEX
-scale = 1
-color = (255,0,0)
-exp = 1
+altura, largura, _ = frame.shape
+video_out = cv.VideoWriter(video_name, codec, 30, (largura, altura))
 
-faces = 0
+if not cap.isOpened():
+    print('Erro ao abrir o vídeo')
+    exit()
 
-for box in boxes:
-    top_left_x = int(box.xyxy.tolist()[0][0])
-    top_left_y = int(box.xyxy.tolist()[0][1])
-    bottom_right_x = int(box.xyxy.tolist()[0][2])
-    bottom_right_y = int(box.xyxy.tolist()[0][3])
+while True:
+    ret, frame = cap.read()
 
-    # aplicando o blur na região de interesse
-    region = img[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
-    img_blur = cv.blur(region, (15,15))
-    img[top_left_y:bottom_right_y, top_left_x:bottom_right_x] = img_blur
+    if not ret:
+        #verificando a leitura do frame
+        break
 
-    #marcando os rostos
-    cv.rectangle(img,(top_left_x,top_left_y),(bottom_right_x,bottom_right_y),(50,200,129),2)
+    results = model(frame)
+    boxes = results[0].boxes
+    # img = cv.imread(frame)
 
-    faces += 1
+    position = (50,50)
+    font = cv.FONT_HERSHEY_COMPLEX
+    scale = 3
+    color = (255,0,0)
+    exp = 2
 
-cv.putText(img, f"{faces} faces", position, font, scale, color, exp)
-print(f'there are {faces} faces')
-cv.imwrite("test.jpg",img)
+    faces = 0
+
+    for box in boxes:
+        top_left_x = int(box.xyxy.tolist()[0][0])
+        top_left_y = int(box.xyxy.tolist()[0][1])
+        bottom_right_x = int(box.xyxy.tolist()[0][2])
+        bottom_right_y = int(box.xyxy.tolist()[0][3])
+
+        # aplicando o blur na região de interesse
+        region = frame[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+        img_blur = cv.blur(region, (50,50))
+        frame[top_left_y:bottom_right_y, top_left_x:bottom_right_x] = img_blur
+
+        #marcando os rostos
+        cv.rectangle(frame,(top_left_x,top_left_y),(bottom_right_x,bottom_right_y),(50,200,129),2)
+
+        faces += 1
+
+    cv.putText(frame, f"{faces} faces", position, font, scale, color, exp)
+    print(f'there are {faces} faces')
+    # cv.imwrite("test.jpg",frame)
+    video_out.write(frame)
+
+video_out.release()
+
+print(f'video criado com sucesso no caminho {video_name}')
