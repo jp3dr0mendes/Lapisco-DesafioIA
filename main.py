05031211bab464455fdb57709f2eb1ac1a1abce0
Pedro <joao.pedro73210@gmail.com>
2023-12-01 03:04:04 -0300

    blur and face number

--- main.py
+++ main.py
@@ -2,31 +2,21 @@ from ultralytics import YOLO
 from PIL import Image
 import cv2 as cv
 
-# # Create a new YOLO model from scratch
-# model = YOLO('yolov8n.yaml')
-
-# # Load a pretrained YOLO model (recommended for training)
-# model = YOLO('yolov8n.pt')
-
-# # Train the model using the 'coco128.yaml' dataset for 3 epochs
-# results = model.train(data='coco128.yaml', epochs=3)
-
-# # Evaluate the model's performance on the validation set
-# results = model.val()
-
-# # Perform object detection on an image using the model
-# results = model('https://ultralytics.com/images/bus.jpg')
-
-# # Export the model to ONNX format
-# success = model.export(format='onnx')
-
 #load model
 model = YOLO("face-detection-yolov8/best.pt")
 # load video
 img_path = 'images.jpg'
+# img_path = 'dynamic_physis_wallpaper.jpg'
+
 results = model(img_path)
 boxes = results[0].boxes
 img = cv.imread(img_path)
+
+position = (20,20)
+font = cv.FONT_HERSHEY_COMPLEX
+scale = 1
+color = (255,0,0)
+exp = 1
 
 faces = 0
 
@@ -36,9 +26,16 @@ for box in boxes:
     bottom_right_x = int(box.xyxy.tolist()[0][2])
     bottom_right_y = int(box.xyxy.tolist()[0][3])
 
+    # aplicando o blur na região de interesse
+    region = img[top_left_y:bottom_right_y, top_left_x:bottom_right_x]
+    img_blur = cv.blur(region, (15,15))
+    img[top_left_y:bottom_right_y, top_left_x:bottom_right_x] = img_blur
+
+    #marcando os rostos
     cv.rectangle(img,(top_left_x,top_left_y),(bottom_right_x,bottom_right_y),(50,200,129),2)
 
     faces += 1
 
+cv.putText(img, f"{faces} faces", position, font, scale, color, exp)
 print(f'there are {faces} faces')
 cv.imwrite("test.jpg",img)
